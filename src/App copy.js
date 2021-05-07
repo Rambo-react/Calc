24b3ebd89ecb1b9e888941c883119d0b9df20d84
Rambo-react <sandstorm0819@gmail.com>
2021-05-07 16:04:37 +0300

    need refactoring

---
+++ src/App copy.js
@@ -0,0 +1,109 @@
+
+import React from 'react';
+import './App.css';
+import DivideButton from './components/DivideButton';
+import MinusButton from './components/MinusButton';
+import MultiplyButton from './components/MultiplyButton';
+import NumberButton from './components/NumberButton';
+import PlusButton from './components/PlusButton';
+
+class App extends React.Component {
+
+  state = {
+    history: [],
+    resultNow: 0,
+    lastSymbolOperator: false,
+    predValue: null,
+    operators: ['+', '-', '*', '/']
+  }
+
+  clickNumberButton = (e) => {
+    console.log('num')
+    this.setState({
+      ...this.state,
+      resultNow: this.state.resultNow === 0 ?
+       e.currentTarget.innerText : this.state.resultNow + e.currentTarget.innerText,
+       lastSymbolOperator: false
+    }) 
+  }
+
+  EqualMath = (stringMath, separator) => {
+    debugger
+    let [firstVal, lastVal] = stringMath.split(separator);
+    return `${Number(firstVal)} ${separator} ${Number(lastVal)}` 
+  }
+
+  clickPlusButton = (e) => {
+    //если в строке есть оператор, то выполняем вычисление
+    debugger
+    console.log('+')
+    
+    //если последний символ не оператор, и строка содержит оператор, но сейчас нажали на оператор, то вычисляем результат
+    if ((!this.state.lastSymbolOperator) && (this.state.resultNow.includes('')))  {
+
+    } else {
+
+    }
+
+    this.setState({
+      ...this.state,
+      predValue: this.state.resultNow,
+      lastSymbolOperator: true,
+      resultNow: this.state.resultNow + "+"
+
+    })
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        <Calculator clickPlusButton={this.clickPlusButton} clickNumberButton={this.clickNumberButton}  resultNow={this.state.resultNow} />
+
+      </div>
+    )
+  }
+
+}
+
+
+
+const Calculator = (props) => {
+
+  return (
+    <div>
+
+      <div>
+        <input />
+        <input placeholder='0' value={props.resultNow} readOnly={true} />
+      </div>
+      <div>
+        <PlusButton clickPlusButton= {props.clickPlusButton}/>
+        <MinusButton />
+        <MultiplyButton />
+        <DivideButton />
+      </div>
+      <div>
+        <NumberButton clickNumberButton={props.clickNumberButton} num={7} />
+        <NumberButton clickNumberButton={props.clickNumberButton} num={8} />
+        <NumberButton clickNumberButton={props.clickNumberButton} num={9} />
+      </div>
+      <div>
+        <NumberButton clickNumberButton={props.clickNumberButton} num={4} />
+        <NumberButton clickNumberButton={props.clickNumberButton} num={5} />
+        <NumberButton clickNumberButton={props.clickNumberButton} num={6} />
+      </div>
+      <div>
+        <NumberButton clickNumberButton={props.clickNumberButton} num={1} />
+        <NumberButton clickNumberButton={props.clickNumberButton} num={2} />
+        <NumberButton clickNumberButton={props.clickNumberButton} num={3} />
+      </div>
+
+    </div>
+  )
+}
+
+
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,111 +1,84 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import DivideButton from './components/DivideButton';
-import MinusButton from './components/MinusButton';
-import MultiplyButton from './components/MultiplyButton';
-import NumberButton from './components/NumberButton';
-import PlusButton from './components/PlusButton';
+// import DivideButton from './components/DivideButton';
+// import MinusButton from './components/MinusButton';
+// import MultiplyButton from './components/MultiplyButton';
+// import NumberButton from './components/NumberButton';
+// import PlusButton from './components/PlusButton';
 
-class App extends React.Component {
+const App = () => {
 
-  state = {
-    history: [],
-    resultNow: 0,
-    lastSymbolOperator: false,
-    predValue: null,
-    operators: ['+', '-', '*', '/']
-  }
+  const [result, setResult] = useState("");
 
-  clickNumberButton = (e) => {
-    console.log('num')
-    this.setState({
-      ...this.state,
-      resultNow: this.state.resultNow === 0 ?
-       e.currentTarget.innerText : this.state.resultNow + e.currentTarget.innerText,
-       lastSymbolOperator: false
-    }) 
-  }
-
-  EqualMath = (stringMath, separator) => {
-    debugger
-    let [firstVal, lastVal] = stringMath.split(separator);
-    return `${Number(firstVal)} ${separator} ${Number(lastVal)}` 
-  }
-
-  clickPlusButton = (e) => {
-    //если в строке есть оператор, то выполняем вычисление
-    debugger
-    console.log('+')
-    
-    //если последний символ не оператор, и строка содержит оператор, но сейчас нажали на оператор, то вычисляем результат
-    if ((!this.state.lastSymbolOperator) && (this.state.resultNow.includes('')))  {
-
-    } else {
-
-    }
-
-    this.setState({
-      ...this.state,
-      predValue: this.state.resultNow,
-      lastSymbolOperator: true,
-      resultNow: this.state.resultNow + "+"
-
-    })
+  const handleClick = (e) => {
+    //условия
+    setResult( result.concat( e.target.innerHTML));
 
   }
 
-
-
-
-  render() {
-    return (
-      <div className="App">
-
-        <Calculator clickPlusButton={this.clickPlusButton} clickNumberButton={this.clickNumberButton}  resultNow={this.state.resultNow} />
-
-      </div>
-    )
+  const clear = () => {
+    setResult("");
   }
 
-}
+  const backspace = () => {
+    setResult(result.slice(0, -1)); // result.slice(0, result.length -1)
+  }
+
+  const calculate = () => {
+    setResult(eval(result).toString());
+  }
 
 
-
-const Calculator = (props) => {
 
   return (
-    <div>
+    <div className="App">
 
-      <div>
-        <input />
-        <input placeholder='0' value={props.resultNow} readOnly={true} />
+      <div className="calculator">
+
+        
+          <input value={result} />
+        
+
+        <div className="keyboard">
+          <button onClick={clear} className="buttonNum" id="clear">Clear</button>
+          <button onClick={backspace} className="buttonNum" id="backspace">C</button>
+          <button onClick={handleClick} className="buttonNum">/</button>
+          <button onClick={handleClick} className="buttonNum">7</button>
+          <button onClick={handleClick} className="buttonNum">8</button>
+          <button onClick={handleClick} className="buttonNum">9</button>
+          <button onClick={handleClick} className="buttonNum">X</button>
+          <button onClick={handleClick} className="buttonNum">4</button>
+          <button onClick={handleClick} className="buttonNum">5</button>
+          <button onClick={handleClick} className="buttonNum">6</button>
+          <button onClick={handleClick} className="buttonNum">-</button>
+          <button onClick={handleClick} className="buttonNum">1</button>
+          <button onClick={handleClick} className="buttonNum">2</button>
+          <button onClick={handleClick} className="buttonNum">3</button>
+          <button onClick={handleClick} className="buttonNum">+</button>
+          <button onClick={handleClick} className="buttonNum">0</button>
+          <button onClick={handleClick} className="buttonNum">.</button>
+          <button onClick={calculate} className="buttonNum" id="equal">=</button>
+        </div>
+
+
       </div>
-      <div>
-        <PlusButton clickPlusButton= {props.clickPlusButton}/>
-        <MinusButton />
-        <MultiplyButton />
-        <DivideButton />
+      <div className="historyBlock">
+      <div>History</div>
+      <div className="history">
+
+       <div className="historyElement">2 +2 = 4</div> 
       </div>
-      <div>
-        <NumberButton clickNumberButton={props.clickNumberButton} num={7} />
-        <NumberButton clickNumberButton={props.clickNumberButton} num={8} />
-        <NumberButton clickNumberButton={props.clickNumberButton} num={9} />
-      </div>
-      <div>
-        <NumberButton clickNumberButton={props.clickNumberButton} num={4} />
-        <NumberButton clickNumberButton={props.clickNumberButton} num={5} />
-        <NumberButton clickNumberButton={props.clickNumberButton} num={6} />
-      </div>
-      <div>
-        <NumberButton clickNumberButton={props.clickNumberButton} num={1} />
-        <NumberButton clickNumberButton={props.clickNumberButton} num={2} />
-        <NumberButton clickNumberButton={props.clickNumberButton} num={3} />
+        
       </div>
 
     </div>
   )
+
 }
+
+
+
 
 
 
