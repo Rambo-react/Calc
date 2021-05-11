335b6fdacacb1f6c638f3e30c8b8103f3db7c1dc
Rambo-react <sandstorm0819@gmail.com>
2021-05-12 00:04:42 +0300

    added event on dotButton click

--- src/App.js
+++ src/App.js
@@ -11,12 +11,16 @@ const App = () => {
   const [secondVal, setSecondVal] = useState(null);
   const [history, setHistory] = useState([]);
   const [lastButtonOperator, setLastButtonOperator] = useState(false);
+  const [lastButtonDot, setLastButtonDot] = useState(false);
+  const [lastButtonEqual, setLastButtonEqual] = useState(false);
 
+
+//цифры
   const handleClick = (e) => {
 
     if (lastButtonOperator) {
       setResult(e.target.innerHTML)
-      // setResult("0");
+      
     } else
       if (result === "0") {
         setResult(e.target.innerHTML)
@@ -27,10 +31,12 @@ const App = () => {
     setLastButtonOperator(false);
   }
 
+  //очистить
   const clear = () => {
     setResult("0");
   }
 
+  //удалить последний символ
   const backspace = () => {
     if (result.length === 1) {
       setResult("0");
@@ -39,10 +45,41 @@ const App = () => {
     }
   }
 
-  const calculate = () => {
-    setResult(eval(result).toString());
-  }
+  //равно
+  const calculate = (e) => {
 
+    //если ничего нету
+    if (firstVal === "") {
+      setFirstVal(result.concat(e.target.innerHTML));
+    } else 
+    if (!lastButtonEqual) {
+      let calcResult;
+      let numFirstVal = Number(firstVal.slice(0, -1));
+      let oper = firstVal.slice(firstVal.length - 1, firstVal.length);
+      let SecondVal = result.includes(".", result.length - 1) ? result.slice(0, -1) : result;
+      let numSecondVal = Number(SecondVal);
+      switch (oper) {
+        case "+": calcResult = numFirstVal + numSecondVal; break;
+        case "-": calcResult = numFirstVal - numSecondVal; break;
+        case "*": calcResult = numFirstVal * numSecondVal; break;
+        case "/": calcResult = numFirstVal / numSecondVal; break;
+      }
+
+      setFirstVal(firstVal + SecondVal + e.target.innerHTML);
+      setResult(String(calcResult));
+      setLastButtonEqual(true);
+    } else {
+
+    }
+    // if (firstVal.includes("=", result.length - 1)) {
+
+    }
+
+    // setResult(eval(result).toString());
+
+  
+
+  //операторы
   const operatorClick = (e) => {
     debugger
     if (!lastButtonOperator) {
@@ -50,82 +87,72 @@ const App = () => {
       let calcResult;
       let oper = firstVal.slice(firstVal.length - 1, firstVal.length);
       let numFirstVal = Number(firstVal.slice(0, -1));
-      let numSecondVal = Number(result);
+
+      // let numSecondVal = Number(result);
+      //если последняий символ разделитель дробной части, то просто откидываем его
+      let SecondVal = result.includes(".", result.length - 1) ? result.slice(0, -1) : result;
+      let numSecondVal = Number(SecondVal);
       switch (oper) {
         case "+": calcResult = numFirstVal + numSecondVal; break;
         case "-": calcResult = numFirstVal - numSecondVal; break;
         case "*": calcResult = numFirstVal * numSecondVal; break;
         case "/": calcResult = numFirstVal / numSecondVal; break;
       }
-      debugger
+      
       if (oper !== "") {
-        setResult(String(calcResult));
         debugger
+        setResult(String(calcResult));
+        
         setFirstVal(String(calcResult).concat(e.target.innerHTML));
       } else {
-        setFirstVal(result.concat(e.target.innerHTML));
+        debugger
+        setFirstVal(SecondVal.concat(e.target.innerHTML));
       }
-      
+
     } else {
-debugger
+      debugger
       setFirstVal(result.concat(e.target.innerHTML));
     }
 
     setLastButtonOperator(true);
   }
 
+  //разделитель целой и дробной части
+  const dotButtonHandler = (e) => {
+    if (lastButtonOperator) {
+      setResult("0.");
+    } else
+      if (!result.includes(".")) {
+        setResult(result.concat(e.target.innerHTML));
+      }
+    setLastButtonOperator(false);
+  }
 
   return (
     <div className="App">
 
       <div className="calculator">
-
         <input value={firstVal} />
         <input value={result} readOnly={true} />
-
-
         <div className="keyboard">
           <Button child={"Clear"} handleClick={clear} />
           <Button child={"C"} handleClick={backspace} />
           <Button child={"/"} handleClick={operatorClick} />
-          {/* <Button child={"/"} handleClick={handleClick} /> */}
           <Button child={"7"} handleClick={handleClick} />
           <Button child={"8"} handleClick={handleClick} />
           <Button child={"9"} handleClick={handleClick} />
           <Button child={"*"} handleClick={operatorClick} />
-          {/* <Button child={"*"} handleClick={handleClick} /> */}
           <Button child={"4"} handleClick={handleClick} />
           <Button child={"5"} handleClick={handleClick} />
           <Button child={"6"} handleClick={handleClick} />
           <Button child={"-"} handleClick={operatorClick} />
-          {/* <Button child={"-"} handleClick={handleClick} /> */}
           <Button child={"1"} handleClick={handleClick} />
           <Button child={"2"} handleClick={handleClick} />
           <Button child={"3"} handleClick={handleClick} />
           <Button child={"+"} handleClick={operatorClick} />
-          {/* <Button child={"+"} handleClick={handleClick} /> */}
           <Button child={"0"} handleClick={handleClick} />
-          <Button child={"."} handleClick={handleClick} />
+          <Button child={"."} handleClick={dotButtonHandler} />
           <Button child={"="} handleClick={calculate} />
-
-          {/* <button onClick={clear} className="buttonNum" id="clear">Clear</button>
-          <button onClick={backspace} className="buttonNum" id="backspace">C</button>
-          <button onClick={handleClick} className="buttonNum">/</button>
-          <button onClick={handleClick} className="buttonNum">7</button>
-          <button onClick={handleClick} className="buttonNum">8</button>
-          <button onClick={handleClick} className="buttonNum">9</button>
-          <button onClick={handleClick} className="buttonNum">X</button>
-          <button onClick={handleClick} className="buttonNum">4</button>
-          <button onClick={handleClick} className="buttonNum">5</button>
-          <button onClick={handleClick} className="buttonNum">6</button>
-          <button onClick={handleClick} className="buttonNum">-</button>
-          <button onClick={handleClick} className="buttonNum">1</button>
-          <button onClick={handleClick} className="buttonNum">2</button>
-          <button onClick={handleClick} className="buttonNum">3</button>
-          <button onClick={handleClick} className="buttonNum">+</button>
-          <button onClick={handleClick} className="buttonNum">0</button>
-          <button onClick={handleClick} className="buttonNum">.</button>
-          <button onClick={calculate} className="buttonNum" id="equal">=</button> */}
         </div>
 
 
